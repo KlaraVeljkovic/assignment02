c9afb57ad8c589a97d0c0f03b3a2b330f9f05ef3
Klara <kveljko1@binghamton.edu>
2024-09-22 15:58:16 -0400

    added

---
+++ class_adapter.java
@@ -0,0 +1,6 @@
+package assignment02;
+import java.util.ArrayList;
+public class ArrayListQueue2<E> extends ArrayList<E> implements Queue<E> 
+{
+ ...
+}
---
+++ object_adapter.java
@@ -0,0 +1,6 @@
+package assignment02;
+import java.util.ArrayList;
+public class ArrayListQueue1<E> implements Queue<E> 
+{
+ ...
+}
