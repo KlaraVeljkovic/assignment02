a0f0a0894cc7036f999140d9b8ee38b0ad266085
Deepanshi Gaur <dgaur1@binghamton.edu>
2024-09-22 16:16:20 -0400

    code

--- class_adapter.java
+++
@@ -1,6 +0,0 @@
-package assignment02;
-import java.util.ArrayList;
-public class ArrayListQueue2<E> extends ArrayList<E> implements Queue<E> 
-{
- ...
-}
--- object_adapter.java
+++
@@ -1,6 +0,0 @@
-package assignment02;
-import java.util.ArrayList;
-public class ArrayListQueue1<E> implements Queue<E> 
-{
- ...
-}
